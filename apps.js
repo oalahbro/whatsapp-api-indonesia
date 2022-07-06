bd3eeae3490bc464409b569fffbc429d5a7eec01
oalahbro <epradana15@gmail.com>
2022-07-06 14:09:37 +0700

    fix post from url

--- apps.js
+++ apps.js
@@ -6,15 +6,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 const { MessageMedia } = require('whatsapp-web.js');
-// const media = new MessageMedia('image/png', "iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAApgAAAKYB3X3/OAAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAANCSURBVEiJtZZPbBtFFMZ/M7ubXdtdb1xSFyeilBapySVU8h8OoFaooFSqiihIVIpQBKci6KEg9Q6H9kovIHoCIVQJJCKE1ENFjnAgcaSGC6rEnxBwA04Tx43t2FnvDAfjkNibxgHxnWb2e/u992bee7tCa00YFsffekFY+nUzFtjW0LrvjRXrCDIAaPLlW0nHL0SsZtVoaF98mLrx3pdhOqLtYPHChahZcYYO7KvPFxvRl5XPp1sN3adWiD1ZAqD6XYK1b/dvE5IWryTt2udLFedwc1+9kLp+vbbpoDh+6TklxBeAi9TL0taeWpdmZzQDry0AcO+jQ12RyohqqoYoo8RDwJrU+qXkjWtfi8Xxt58BdQuwQs9qC/afLwCw8tnQbqYAPsgxE1S6F3EAIXux2oQFKm0ihMsOF71dHYx+f3NND68ghCu1YIoePPQN1pGRABkJ6Bus96CutRZMydTl+TvuiRW1m3n0eDl0vRPcEysqdXn+jsQPsrHMquGeXEaY4Yk4wxWcY5V/9scqOMOVUFthatyTy8QyqwZ+kDURKoMWxNKr2EeqVKcTNOajqKoBgOE28U4tdQl5p5bwCw7BWquaZSzAPlwjlithJtp3pTImSqQRrb2Z8PHGigD4RZuNX6JYj6wj7O4TFLbCO/Mn/m8R+h6rYSUb3ekokRY6f/YukArN979jcW+V/S8g0eT/N3VN3kTqWbQ428m9/8k0P/1aIhF36PccEl6EhOcAUCrXKZXXWS3XKd2vc/TRBG9O5ELC17MmWubD2nKhUKZa26Ba2+D3P+4/MNCFwg59oWVeYhkzgN/JDR8deKBoD7Y+ljEjGZ0sosXVTvbc6RHirr2reNy1OXd6pJsQ+gqjk8VWFYmHrwBzW/n+uMPFiRwHB2I7ih8ciHFxIkd/3Omk5tCDV1t+2nNu5sxxpDFNx+huNhVT3/zMDz8usXC3ddaHBj1GHj/As08fwTS7Kt1HBTmyN29vdwAw+/wbwLVOJ3uAD1wi/dUH7Qei66PfyuRj4Ik9is+hglfbkbfR3cnZm7chlUWLdwmprtCohX4HUtlOcQjLYCu+fzGJH2QRKvP3UNz8bWk1qMxjGTOMThZ3kvgLI5AzFfo379UAAAAASUVORK5CYII=");
-// app.use(express.json());
-// console.log(media)
+const fetch = require('node-fetch');
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
-// // app.use(bodyParser.json());
-// const parser = bodyParser.urlencoded({extended:false})
 
-
+const generateRandomString = (myLength) => {
+    const chars =
+      "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+    const randomArray = Array.from(
+      { length: myLength },
+      (v, k) => chars[Math.floor(Math.random() * chars.length)]
+    );
+  
+    const randomString = randomArray.join("");
+    return randomString;
+  };
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: { headless: true },
@@ -23,38 +30,49 @@ const client = new Client({
 client.initialize();
 client.on('qr', (qr) => {
     qrcode.generate(qr);
-});``
+}); ``
 client.on('authenticated', () => {
     console.log('AUTHENTICATED');
 });
 client.on('auth_failure', msg => {
-    // Fired if session restore was unsuccessful
+    // Fired if session restore was unsuccessful 
     console.error('AUTHENTICATION FAILURE', msg);
 });
 
 client.on('ready', () => {
+    
     console.log('Client is ready!');
 });
 
-app.post('/kirim',async(req, res) => {
+app.post('/kirim', async (req, res) => {
 
     let pesan = req.body.pesan
     let nomer = req.body.nomer
-    // res.send(peslet sessionData;a
-    res.send(req.body)
-    const media = await MessageMedia.fromUrl('http://pii.or.id/uploads/dummies.pdf',);
-
+    let nama = generateRandomString(10)
+    let url = req.body.link
     
-    client.sendMessage(nomer + "@c.us", media, {caption: pesan})
- 
+        // res.send(peslet sessionData;a
+    res.send(req.body)
+    const response = await fetch(url);
+    const buffer = await response.buffer();
+        fs.writeFile('./image/'+nama+'.jpg', buffer, () => {
+            let media = MessageMedia.fromFilePath('./image/'+nama+'.jpg')
+            client.sendMessage(nomer + "@c.us", media, { caption: pesan })
+            console.log('finished downloading!')});
+
+    // const media = await MessageMedia.fromUrl('https://www.iasgurukul.com/images/abhinav.png',);
+
 
     // //console.log(req.body)
     console.log(nomer + "@c.us")
     console.log(pesan)
+    console.log(url)
     // res.sendStatus(200);
 })
 
 app.listen(port, () => {
+        
+      
     console.log(`Example app listening on port ${port}`)
 })
 client.on('message', msg => {
