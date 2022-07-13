c84f1c9e2a3d18cdba97ebe23905e0955cbac0ef
salsahh01 <salsajktmgt@gmail.com>
2022-07-13 09:06:58 +0700

    otp

--- apps.js
+++ apps.js
@@ -13,15 +13,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const generateRandomString = (myLength) => {
     const chars =
-      "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+        "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
     const randomArray = Array.from(
-      { length: myLength },
-      (v, k) => chars[Math.floor(Math.random() * chars.length)]
+        { length: myLength },
+        (v, k) => chars[Math.floor(Math.random() * chars.length)]
     );
-  
+
     const randomString = randomArray.join("");
     return randomString;
-  };
+};
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: { headless: true },
@@ -39,7 +39,7 @@ client.on('auth_failure', msg => {
 });
 
 client.on('ready', () => {
-    
+
     console.log('Client is ready!');
 });
 
@@ -49,34 +49,34 @@ app.post('/kirim', async (req, res) => {
     let nomer = req.body.nomer
     let nama = generateRandomString(10)
     let url = req.body.link
-    
+
     var phoneno = /^(62)8[1-9][0-9]{6,10}$/;
-    if(nomer.match(phoneno)) {
+    if (nomer.match(phoneno)) {
         const response = await fetch(url);
         const buffer = await response.buffer();
-        fs.writeFile('./image/'+nama+'.jpg', buffer, () => {
-            let media = MessageMedia.fromFilePath('./image/'+nama+'.jpg')
+        fs.writeFile('./image/' + nama + '.jpg', buffer, () => {
+            let media = MessageMedia.fromFilePath('./image/' + nama + '.jpg')
             client.sendMessage(nomer + "@c.us", media, { caption: pesan })
             console.log('finished downloading!')
-        
+
             try {
-                fs.unlinkSync('./image/'+nama+'.jpg')
+                fs.unlinkSync('./image/' + nama + '.jpg')
                 //file removed
-              } catch(err) {
+            } catch (err) {
                 console.error(err)
-              }
+            }
         });
         // res.send(req.body)
-        res.json({status:'sukses'})
-      console.log('sukses')
-    }  
-    else {  
+        res.json({ status: 'sukses' })
+        console.log('sukses')
+    }
+    else {
         console.log('gagal')
         res.send('gagal')
     }
-        
-    
-    
+
+
+
 
     // const media = await MessageMedia.fromUrl('https://www.iasgurukul.com/images/abhinav.png',);
 
@@ -92,40 +92,62 @@ app.post('/kirimpdf', async (req, res) => {
     let nomer = req.body.nomer
     let nama = generateRandomString(10)
     let url = req.body.link
-    
+
     var phoneno = /^(62)8[1-9][0-9]{6,10}$/;
-    if(nomer.match(phoneno)) {
+    if (nomer.match(phoneno)) {
         const response = await fetch(url);
         const buffer = await response.buffer();
-        fs.writeFile('./pdf/'+nama+'.pdf', buffer, () => {
-            let media = MessageMedia.fromFilePath('./pdf/'+nama+'.pdf')
+        fs.writeFile('./pdf/' + nama + '.pdf', buffer, () => {
+            let media = MessageMedia.fromFilePath('./pdf/' + nama + '.pdf')
             client.sendMessage(nomer + "@c.us", media, { caption: pesan })
             console.log('finished downloading!')
-        
+
             try {
-                fs.unlinkSync('./pdf/'+nama+'.pdf')
-              } catch(err) {
+                fs.unlinkSync('./pdf/' + nama + '.pdf')
+            } catch (err) {
                 console.error(err)
-              }
+            }
         });
         // res.send(req.body)
-        res.json({status:'sukses'})
-      console.log('sukses')
-    }  
-    else {  
+        res.json({ status: 'sukses' })
+        console.log('sukses')
+    }
+    else {
         console.log('gagal')
         res.send('gagal')
     }
-        
+
     console.log(nomer + "@c.us")
     console.log(pesan)
     console.log(url)
 })
 
+app.post('/otp', (req, res) => {
+
+    let pesan = req.body.pesan
+    let nomer = req.body.nomer
+
+    var phoneno = /^(62)8[1-9][0-9]{6,10}$/;
+    if (nomer.match(phoneno)) {
+
+        client.sendMessage(nomer + "@c.us", pesan)
+
+        // res.send(req.body)
+        res.json({ status: 'sukses' })
+        console.log('sukses')
+    }
+    else {
+        console.log('gagal')
+        res.send('gagal')
+    }
+
+    console.log(nomer + "@c.us")
+    console.log(pesan)
+})
 
 app.listen(port, () => {
-        
-      
+
+
     console.log(`Example app listening on port ${port}`)
 })
 client.on('message', msg => {
